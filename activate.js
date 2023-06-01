046b6aa7ce86d7a268afa675de66c4980426df29
Maximiliano Valin <maximiliano.valin@sysdig.com>
2023-06-01 07:23:34 -0600

    Fix issue when user and password and tab was not closed

--- activate.js
+++ activate.js
@@ -1,13 +1,17 @@
-setTimeout(function () {
+setInterval(function () {
     if (/https:\/\/.*\.okta\.com\/activate/.test(window.location.href)) {
         const button = document.querySelector('input[type="submit"]')
         const label = document.querySelector(".okta-form-title")
         if (button) {
-            button.click();
+            if (button.value == 'Next') {
+                console.log("Button clicked")
+                button.click();
+            }
         }
         if (label) {
             if (label.innerText == "Device activated") {
                 setTimeout(function () {
+                    console.log("Label found")
                     chrome.runtime.sendMessage({}, function () { })
                 }, 1000)
             }
