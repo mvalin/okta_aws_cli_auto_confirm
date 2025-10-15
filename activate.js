c158072047c8fed7aaa6ec959bf266dcf59dd073
Maximiliano Valin <maximiliano.valin@sysdig.com>
2025-10-15 10:49:12 -0600

    Add support for aws-sso-util auto confirm webpage

--- activate.js
+++ activate.js
@@ -15,4 +15,26 @@ setInterval(function () {
             }
         }
     }
+    if (/https:\/\/.*\.awsapps\.com\/start\/.*/.test(window.location.href)) {
+        const buttonAllow = document.querySelector('button[data-testid="allow-access-button"]')
+        const buttonConfirm = document.querySelector('button[id="cli_verification_btn"]')
+        const message = document.querySelector('div[id="alert-:ri:"]')
+        if (buttonConfirm) {
+            if (buttonConfirm.innerText == "Confirm and continue") {
+                buttonConfirm.click();
+            }
+        }
+        if (buttonAllow) {
+            if (buttonAllow.innerText == "Allow") {
+                buttonAllow.click();
+            }
+        }
+        if (message) {
+            if (message.innerText.includes("You have approved the request for access.")) {
+                setTimeout(function () {
+                    chrome.runtime.sendMessage({}, function () { })
+                }, 1000)
+            }
+        }
+    }
 }, 1000);
